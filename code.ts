5deb572699546d4fe3ef8301a5e77f34c9c82add
jinkang-0 <jinkang.fang@gmail.com>
2024-12-09 12:56:44 -0800

    added support for variables

--- code.ts
+++ code.ts
@@ -1,15 +1,28 @@
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { height: 400, width: 400 });
+figma.showUI(__html__, { height: 800, width: 400 });
 
-interface GenerateColorMessage {
-  type: "generate-colors";
+interface GenerateColorStyleMessage {
+  type: "generate-colors-style";
   colors: string[];
   folderName: string;
   makeRectangles: boolean;
 }
 
+interface GenerateColorVariableMessage {
+  type: "generate-colors-variable";
+  colors: string[];
+  folderName: string;
+  makeRectangles: boolean;
+  collectionName?: string;
+  variableMode?: string;
+}
+
+type GenerateColorMessage =
+  | GenerateColorStyleMessage
+  | GenerateColorVariableMessage;
+
 // input: #001122 or #00112233
-const hexToRGBA = (hexCode: string) => {
+const hexToPaint = (hexCode: string) => {
   const hex = hexCode.replace("#", "");
   let opacity = 1;
 
@@ -30,7 +43,32 @@ const hexToRGBA = (hexCode: string) => {
   const g = parseInt(gText, 16) / 255;
   const b = parseInt(bText, 16) / 255;
 
-  return { color: { r, g, b }, opacity };
+  const paint: SolidPaint = {
+    type: "SOLID",
+    color: {
+      r,
+      g,
+      b
+    },
+    opacity
+  };
+
+  return paint;
+};
+
+const paintToRGBA = (paint: SolidPaint): RGBA => {
+  return { ...paint.color, a: paint.opacity ?? 1 };
+};
+
+const getColorName = (idx: number) => {
+  const prefix = idx >= 12 ? "a" : "";
+  const number = (idx % 12) + 1;
+  return `${prefix}${number}`;
+};
+
+const getColorPath = (folderName: string, idx: number) => {
+  const color = getColorName(idx);
+  return folderName ? `${folderName}/${color}` : color;
 };
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
@@ -39,16 +77,16 @@ const hexToRGBA = (hexCode: string) => {
 figma.ui.onmessage = async (msg: GenerateColorMessage | undefined) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg) {
+  if (!msg) return;
+
+  if (msg.type == "generate-colors-style") {
     const nodes: SceneNode[] = [];
 
     for (let i = 0; i < 24; i++) {
       // create color style
       const style = figma.createPaintStyle();
-      const prefix = i >= 12 ? "a" : "";
-      const number = (i % 12) + 1;
-      style.name = `${msg.folderName}/${prefix}${number}`;
-      style.paints = [{ type: "SOLID", ...hexToRGBA(msg.colors[i]) }];
+      style.name = getColorPath(msg.folderName, i);
+      style.paints = [hexToPaint(msg.colors[i])];
 
       // create rect
       if (msg.makeRectangles) {
@@ -56,8 +94,102 @@ figma.ui.onmessage = async (msg: GenerateColorMessage | undefined) => {
         rect.x = figma.viewport.center.x + (i % 12) * 100;
         rect.y = figma.viewport.center.y + Math.floor(i / 12) * 52;
         rect.resize(96, 48);
-        rect.name = `${prefix}${number}`;
+        rect.name = getColorName(i);
         await rect.setFillStyleIdAsync(style.id);
+
+        nodes.push(rect);
+      }
+    }
+
+    // group rects and select
+    if (msg.makeRectangles) {
+      const group = figma.group(nodes, figma.currentPage);
+      group.name = msg.folderName;
+      figma.currentPage.selection = [group];
+    }
+  } else if (msg.type == "generate-colors-variable") {
+    const nodes: SceneNode[] = [];
+
+    // determine collection
+    const collectionName = msg.collectionName || "Collection 1";
+    let varCollection: VariableCollection | undefined;
+
+    try {
+      const collections =
+        await figma.variables.getLocalVariableCollectionsAsync();
+      varCollection = collections.find((c) => c.name === collectionName);
+      if (!varCollection) {
+        varCollection =
+          figma.variables.createVariableCollection(collectionName);
+      }
+    } catch {
+      varCollection = figma.variables.createVariableCollection(collectionName);
+    }
+
+    // determine mode
+    let varMode = "";
+    if (!msg.variableMode) {
+      varMode = varCollection.modes[0].modeId;
+    } else {
+      const foundMode = varCollection.modes.find((v) => v.name === varMode);
+      if (foundMode) {
+        varMode = foundMode.modeId;
+      } else {
+        // attempt to add mode, fail gracefully and notify client
+        try {
+          varMode = varCollection.addMode(varMode);
+        } catch {
+          const errorMessage = `Failed to add mode to ${collectionName}.`;
+          console.error(errorMessage);
+          figma.ui.postMessage({
+            type: "server-error",
+            message: errorMessage
+          });
+          return;
+        }
+      }
+    }
+
+    for (let i = 0; i < 24; i++) {
+      // create color variable
+      const colorName = getColorPath(msg.folderName, i);
+
+      let variable: Variable | undefined;
+      try {
+        variable = figma.variables.createVariable(
+          colorName,
+          varCollection,
+          "COLOR"
+        );
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : String(error);
+        console.error(errorMessage);
+        figma.ui.postMessage({
+          type: "server-error",
+          message: errorMessage
+        });
+        return;
+      }
+
+      variable.setValueForMode(varMode, paintToRGBA(hexToPaint(msg.colors[i])));
+
+      // assign color
+      const fillArray = [hexToPaint(msg.colors[i])];
+      fillArray[0] = figma.variables.setBoundVariableForPaint(
+        hexToPaint(msg.colors[i]),
+        "color",
+        variable
+      );
+
+      // create rect
+      if (msg.makeRectangles) {
+        const rect = figma.createRectangle();
+        rect.x = figma.viewport.center.x + (i % 12) * 100;
+        rect.y = figma.viewport.center.y + Math.floor(i / 12) * 52;
+        rect.resize(96, 48);
+        rect.name = getColorName(i);
+        rect.fills = fillArray;
 
         nodes.push(rect);
       }
